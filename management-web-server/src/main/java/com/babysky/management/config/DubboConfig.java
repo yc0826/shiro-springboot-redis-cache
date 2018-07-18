8c8113e225ab03164ed7ed6c532516a79a083fb5
YangChao <yangchao@babysky.com.cn>
2018-07-18 14:40:50 +0800

    mybatis 和 dubbo都切换成最新得springboot版本 完成了配置启动

---
+++ management-web-server/src/main/java/com/babysky/management/config/DubboConfig.java
@@ -0,0 +1,45 @@
+package com.babysky.management.config;
+
+import com.alibaba.dubbo.config.ConsumerConfig;
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.babysky.management.api.auth.service.api.CfgSysResoService;
+import com.babysky.management.api.auth.service.api.MstInterUserBaseService;
+import com.babysky.management.api.auth.service.api.MstRollBaseService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author YangChao
+ */
+@Configuration
+public class DubboConfig {
+
+    @Reference
+    private MstRollBaseService roleService;
+    @Reference
+    private CfgSysResoService resourceService;
+    @Reference
+    private MstInterUserBaseService userService;
+
+    @Bean
+    public MstRollBaseService getRoleService() {
+        return roleService;
+    }
+
+    @Bean
+    public CfgSysResoService getResourceService() {
+        return resourceService;
+    }
+
+    @Bean
+    public MstInterUserBaseService getUserService() {
+        return userService;
+    }
+
+    @Bean
+    public ConsumerConfig consumerConfig() {
+        ConsumerConfig consumerConfig = new ConsumerConfig();
+        consumerConfig.setTimeout(30000);
+        return consumerConfig;
+    }
+}
