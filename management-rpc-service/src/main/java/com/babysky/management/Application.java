3345b78064e7bca3daa1bdafe7a23dff06419df1
YangChao <yangchao@babysky.com.cn>
2018-07-18 11:09:07 +0800

    mybatis 和 dubbo都切换成最新得springboot版本 完成了配置启动

--- management-rpc-service/src/main/java/com/babysky/management/Application.java
+++ management-rpc-service/src/main/java/com/babysky/management/Application.java
@@ -1,6 +1,5 @@
 package com.babysky.management;
 
-import com.alibaba.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author YangChao
  */
 @SpringBootApplication
-@DubboComponentScan(basePackages = "com.babysky.management.service.impl")
 public class Application {
 
     public static void main(String[] args) {
