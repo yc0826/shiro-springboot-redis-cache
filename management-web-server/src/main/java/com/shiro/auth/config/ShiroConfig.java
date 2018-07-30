89d64ade44e7dd61637040c34d14ee4261533a76
YangChao <yangchao@babysky.com.cn>
2018-07-30 11:07:20 +0800

    修改包名，使更加通用

--- management-web-server/src/main/java/com/shiro/auth/config/ShiroConfig.java
+++ management-web-server/src/main/java/com/shiro/auth/config/ShiroConfig.java
@@ -1,22 +1,37 @@
 package com.shiro.auth.config;
 
+import com.shiro.auth.shiro.authc.credential.CustomCredentialsMatcher;
+import com.shiro.auth.shiro.cache.JedisManager;
+import com.shiro.auth.shiro.cache.JedisShiroSessionRepository;
+import com.shiro.auth.shiro.cache.impl.CustomShiroCacheManager;
+import com.shiro.auth.shiro.cache.impl.JedisShiroCacheManager;
+import com.shiro.auth.shiro.dao.CustomShiroSessionDAO;
 import com.shiro.auth.shiro.filter.AccessControlFilter;
 import com.shiro.auth.shiro.filter.KickoutSessionControlFilter;
 import com.shiro.auth.shiro.filter.ServerFormAuthenticationFilter;
 import com.shiro.auth.shiro.filter.SimpleFilter;
-import org.apache.shiro.mgt.SecurityManager;
+import com.shiro.auth.shiro.realm.UserRealm;
+import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.servlet.SimpleCookie;
+import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.web.filter.DelegatingFilterProxy;
+import redis.clients.jedis.JedisPool;
+
+import javax.servlet.Filter;
+import java.util.Map;
 
 /**
  * @author YangChao
  */
 @Configuration
-@ImportResource({"classpath:shiro/*.xml"})
 public class ShiroConfig {
 
     @Bean
@@ -62,137 +77,159 @@ public class ShiroConfig {
     }
 
     @Bean
-    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(SecurityManager securityManager) {
+    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(JedisPool jedisPool) {
         AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor
                 = new AuthorizationAttributeSourceAdvisor();
-        authorizationAttributeSourceAdvisor.setSecurityManager(securityManager);
+        authorizationAttributeSourceAdvisor.setSecurityManager(securityManager(jedisPool));
         return authorizationAttributeSourceAdvisor;
     }
 
-//
-//
-//    @Bean
-//    public SimpleCookie sessionIdCookie() {
-//        SimpleCookie simpleCookie = new SimpleCookie();
-//        simpleCookie.setName("babysky");
-//        simpleCookie.setHttpOnly(true);
-//        simpleCookie.setMaxAge(-1);
-//        return simpleCookie;
-//    }
-//
-//    @Bean
-//    public LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
-//        return new LifecycleBeanPostProcessor();
-//    }
-//
-//    @Bean
-//    public JedisManager jedisManager() {
-//        JedisManager jedisManager = new JedisManager();
-//        jedisManager.setJedisPool( jedisPool());
-//        return jedisManager;
-//    }
-//
-//    @Bean
-//    public JedisShiroSessionRepository jedisShiroSessionRepository() {
-//        JedisShiroSessionRepository jedisShiroSessionRepository = new JedisShiroSessionRepository();
-//        jedisShiroSessionRepository.setJedisManager(jedisManager());
-//        return jedisShiroSessionRepository;
-//    }
-//
-//    @Bean
-//    public JedisShiroCacheManager jedisShiroCacheManager() {
-//        JedisShiroCacheManager jedisShiroCacheManager = new JedisShiroCacheManager();
-//        jedisShiroCacheManager.setJedisManager(jedisManager());
-//        return jedisShiroCacheManager;
-//    }
-//
-//    @Bean
-//    public CustomShiroCacheManager cacheManager() {
-//        CustomShiroCacheManager cacheManager = new CustomShiroCacheManager();
-//        cacheManager.setShiroCacheManager(jedisShiroCacheManager());
-//        return cacheManager;
-//    }
-//
-//    @Bean
-//    public CustomShiroSessionDAO customShiroSessionDAO() {
-//        CustomShiroSessionDAO customShiroSessionDAO = new CustomShiroSessionDAO();
-//        customShiroSessionDAO.setShiroSessionRepository(jedisShiroSessionRepository());
-//        return customShiroSessionDAO;
-//    }
-//
-//    @Bean
-//    public DefaultWebSessionManager sessionManager() {
-//        DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
-//        sessionManager.setSessionValidationInterval(1800000);
-//        sessionManager.setGlobalSessionTimeout(1800000);
-//        sessionManager.setSessionIdCookie(sessionIdCookie());
-//        sessionManager.setSessionDAO(customShiroSessionDAO());
-//        sessionManager.setDeleteInvalidSessions(true);
-//        return sessionManager;
-//    }
-//
-//    @Bean
-//    public CustomCredentialsMatcher credentialsMatcher() {
-//        return new CustomCredentialsMatcher();
-//    }
-//
-//    @Bean
-//    public UserRealm userRealm() {
-//        UserRealm userRealm = new UserRealm();
-//        userRealm.setCredentialsMatcher(credentialsMatcher());
-//        userRealm.setCachingEnabled(false);
-//        return userRealm;
-//    }
-//
-//    @Bean
-//    public DefaultWebSecurityManager securityManager() {
-//        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
-//        securityManager.setRealm(userRealm());
-//        securityManager.setSessionManager(sessionManager());
-//        securityManager.setCacheManager(cacheManager());
-//        return securityManager;
-//    }
-//
-//    @Bean
-//    public ServerFormAuthenticationFilter formAuthenticationFilter() {
-//        ServerFormAuthenticationFilter formAuthenticationFilter = new ServerFormAuthenticationFilter();
-//        formAuthenticationFilter.setUsernameParam("username");
-//        formAuthenticationFilter.setPasswordParam("password");
-//        return formAuthenticationFilter;
-//    }
-//
-//    @Bean
-//    public SimpleFilter simpleFilter() {
-//        return new SimpleFilter();
-//    }
-//
-//    @Bean
-//    public AccessControlFilter accessControlFilter() {
-//        return new AccessControlFilter();
-//    }
-//
-//    @Bean("shiroFilter")
-//    public ShiroFilterFactoryBean shiroFilterFactoryBean() {
-//        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
-//        shiroFilterFactoryBean.setSecurityManager(securityManager());
-//        shiroFilterFactoryBean.setLoginUrl("/login");
-//        Map<String, Filter> filterMap = shiroFilterFactoryBean.getFilters();
-//        filterMap.put("authc", formAuthenticationFilter());
-//        filterMap.put("accessControl", accessControlFilter());
-//        filterMap.put("simple", simpleFilter());
-//
-//        Map<String, String> filterChainDefinitionMap = shiroFilterFactoryBean.getFilterChainDefinitionMap();
-//
-//        filterChainDefinitionMap.put("/login", "authc");
-//        filterChainDefinitionMap.put("/logout", "logout");
-//        filterChainDefinitionMap.put("/static/**", "anon");
-//        filterChainDefinitionMap.put("/common/**", "anon");
-//        filterChainDefinitionMap.put("/**", "accessControl,simple");
-//        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
-//        return shiroFilterFactoryBean;
-//    }
-//
-//
+
+
+    @Bean
+    public SimpleCookie sessionIdCookie() {
+        SimpleCookie simpleCookie = new SimpleCookie();
+        simpleCookie.setName("shiroAuth");
+        simpleCookie.setHttpOnly(true);
+        simpleCookie.setMaxAge(-1);
+        return simpleCookie;
+    }
+
+    @Bean
+    public LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
+        return new LifecycleBeanPostProcessor();
+    }
+
+    @Bean
+    public JedisManager jedisManager(JedisPool jedisPool) {
+        JedisManager jedisManager = new JedisManager();
+        jedisManager.setJedisPool(jedisPool);
+        return jedisManager;
+    }
+
+    @Bean
+    public JedisShiroSessionRepository jedisShiroSessionRepository(JedisPool jedisPool) {
+        JedisShiroSessionRepository jedisShiroSessionRepository = new JedisShiroSessionRepository();
+        jedisShiroSessionRepository.setJedisManager(jedisManager(jedisPool));
+        return jedisShiroSessionRepository;
+    }
+
+    @Bean
+    public JedisShiroCacheManager jedisShiroCacheManager(JedisPool jedisPool) {
+        JedisShiroCacheManager jedisShiroCacheManager = new JedisShiroCacheManager();
+        jedisShiroCacheManager.setJedisManager(jedisManager(jedisPool));
+        return jedisShiroCacheManager;
+    }
+
+    @Bean
+    public CustomShiroCacheManager cacheManager(JedisPool jedisPool) {
+        CustomShiroCacheManager cacheManager = new CustomShiroCacheManager();
+        cacheManager.setShiroCacheManager(jedisShiroCacheManager(jedisPool));
+        return cacheManager;
+    }
+
+    @Bean
+    public CustomShiroSessionDAO customShiroSessionDAO(JedisPool jedisPool) {
+        CustomShiroSessionDAO customShiroSessionDAO = new CustomShiroSessionDAO();
+        customShiroSessionDAO.setShiroSessionRepository(jedisShiroSessionRepository(jedisPool));
+        return customShiroSessionDAO;
+    }
+
+    @Bean
+    public DefaultWebSessionManager sessionManager(JedisPool jedisPool) {
+        DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
+        sessionManager.setSessionValidationInterval(1800000);
+        sessionManager.setGlobalSessionTimeout(1800000);
+        sessionManager.setSessionIdCookie(sessionIdCookie());
+        sessionManager.setSessionDAO(customShiroSessionDAO(jedisPool));
+        sessionManager.setDeleteInvalidSessions(true);
+        return sessionManager;
+    }
+
+    @Bean
+    public CustomCredentialsMatcher credentialsMatcher() {
+        return new CustomCredentialsMatcher();
+    }
+
+    @Bean
+    public UserRealm userRealm() {
+        UserRealm userRealm = new UserRealm();
+        userRealm.setCredentialsMatcher(credentialsMatcher());
+        userRealm.setCachingEnabled(false);
+        return userRealm;
+    }
+
+    @Bean
+    public DefaultWebSecurityManager securityManager(JedisPool jedisPool) {
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        securityManager.setRealm(userRealm());
+        securityManager.setSessionManager(sessionManager(jedisPool));
+        securityManager.setCacheManager(cacheManager(jedisPool));
+        return securityManager;
+    }
+
+    @Bean
+    public ServerFormAuthenticationFilter formAuthenticationFilter() {
+        ServerFormAuthenticationFilter formAuthenticationFilter = new ServerFormAuthenticationFilter();
+        formAuthenticationFilter.setUsernameParam("username");
+        formAuthenticationFilter.setPasswordParam("password");
+        return formAuthenticationFilter;
+    }
+
+    @Bean
+    public SimpleFilter simpleFilter() {
+        return new SimpleFilter();
+    }
+
+    @Bean
+    public AccessControlFilter accessControlFilter() {
+        return new AccessControlFilter();
+    }
+
+    @Bean
+    public KickoutSessionControlFilter kickoutSessionControlFilter(JedisPool jedisPool) {
+        KickoutSessionControlFilter kickoutSessionControlFilter = new KickoutSessionControlFilter();
+        kickoutSessionControlFilter.setCacheManager(cacheManager(jedisPool));
+        return kickoutSessionControlFilter;
+    }
+
+    @Bean("shiroFilter")
+    public ShiroFilterFactoryBean shiroFilterFactoryBean(JedisPool jedisPool) {
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        shiroFilterFactoryBean.setSecurityManager(securityManager(jedisPool));
+        shiroFilterFactoryBean.setLoginUrl("/login");
+        Map<String, Filter> filterMap = shiroFilterFactoryBean.getFilters();
+        filterMap.put("authc", formAuthenticationFilter());
+        filterMap.put("accessControl", accessControlFilter());
+        filterMap.put("simple", simpleFilter());
+        filterMap.put("kickout", kickoutSessionControlFilter(jedisPool));
+
+        Map<String, String> filterChainDefinitionMap = shiroFilterFactoryBean.getFilterChainDefinitionMap();
+
+        filterChainDefinitionMap.put("/login", "authc");
+        filterChainDefinitionMap.put("/logout", "logout");
+        filterChainDefinitionMap.put("/static/**", "anon");
+        filterChainDefinitionMap.put("/common/**", "anon");
+        filterChainDefinitionMap.put("/**", "kickout, accessControl,simple");
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
+        return shiroFilterFactoryBean;
+    }
+
+
+    /**
+     * 下面的代码是添加注解支持
+     */
+    @Bean
+    @DependsOn("lifecycleBeanPostProcessor")
+    public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
+        DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator = new DefaultAdvisorAutoProxyCreator();
+
+        // 强制使用cglib，防止重复代理和可能引起代理出错的问题
+        // https://zhuanlan.zhihu.com/p/29161098
+        defaultAdvisorAutoProxyCreator.setProxyTargetClass(true);
+        return defaultAdvisorAutoProxyCreator;
+    }
+
 
 
 }
